69166309c52ad3b12cad115900c58cf1cee9e0de
Paras Panchal <paraspanchal5555@gmail.com>
2023-12-08 22:24:52 +0530

    Add project files.

---
+++ LinearSearchAlgorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] Gojo = { 1, 2, 3, 4, 5, 6 };
+
+int target = 3;
+
+int result = Array.IndexOf(Gojo, target);
+
+Console.WriteLine($"Element {target} is found at index {result}");
+
+
+
